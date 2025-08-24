c49e7b6d141a0266449f6c988a7b11e8683a8933
Kaushal <sonawanekaushal05@gmail.com>
2025-08-24 23:32:10 +0530

    new certification added

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -2,28 +2,29 @@ import type { Certificate } from "../components/Certifications";
 
 export const certificates: Certificate[] = [
   {
-    id: "4",
-    title: "Certificate of Experience – YWS",
-    imageUrl: "/certificates/YWSintership.jpg",
-    downloadUrl: "/certificates/YWSintership.pdf"
+    id: "1",
+    title: "AI Foundation Associate - Oracle",
+    imageUrl: "/certificates/Oracle_AI_Foundation_Associate.jpg",
+    downloadUrl: "/certificates/Oracle_AI_Foundation_Associate.pdf"
   },
+
   {
     id: "2",
-    title: "AWS Summit – Certificate of Attendance",
-    imageUrl: "/certificates/aws_summit_certificate.jpeg",
-    downloadUrl: "/certificates/aws_summit_certificate.pdf"
-  },
-  {
-    id: "3",
     title: "Software Engineer Intern - HackerRank Certificate",
     imageUrl: "/certificates/software_engineer_intern certificate.jpg",
     downloadUrl: "/certificates/software_engineer_intern certificate.pdf"
   },
   {
-    id: "11",
-    title: "CSS Certification – HackerRank",
-    imageUrl: "/certificates/css_hackerrank.jpg",
-    downloadUrl: "/certificates/css_hackerrank.pdf"
+    id: "3",
+    title: "Certificate of Experience – YWS",
+    imageUrl: "/certificates/YWSintership.jpg",
+    downloadUrl: "/certificates/YWSintership.pdf"
+  },
+  {
+    id: "4",
+    title: "AWS Summit – Certificate of Attendance",
+    imageUrl: "/certificates/aws_summit_certificate.jpeg",
+    downloadUrl: "/certificates/aws_summit_certificate.pdf"
   },
   {
     id: "5",
@@ -33,15 +34,15 @@ export const certificates: Certificate[] = [
   },
   {
     id: "6",
-    title: "Data Analytics Certificate – Forage",
-    imageUrl: "/certificates/data_analytics_forage.jpg",
-    downloadUrl: "/certificates/data_analytics_forage.pdf"
+    title: "Software Development Workshop – Crio.do",
+    imageUrl: "/certificates/software_dev_workshop_crio.jpg",
+    downloadUrl: "/certificates/software_dev_workshop_crio.pdf"
   },
   {
     id: "7",
-    title: "Data Science & AI/ML Internship – YBI Foundation",
-    imageUrl: "/certificates/data_science_aiml_ybi.jpg",
-    downloadUrl: "/certificates/data_science_aiml_ybi.pdf"
+    title: "ServiceNow Micro-Certification",
+    imageUrl: "/certificates/servicenow_micro.jpeg",
+    downloadUrl: "/certificates/servicenow_micro.pdf"
   },
   {
     id: "8",
@@ -51,24 +52,33 @@ export const certificates: Certificate[] = [
   },
   {
     id: "9",
+    title: "Data Analytics Certificate – Forage",
+    imageUrl: "/certificates/data_analytics_forage.jpg",
+    downloadUrl: "/certificates/data_analytics_forage.pdf"
+  },
+  {
+    id: "10",
+    title: "Data Science & AI/ML Internship – YBI Foundation",
+    imageUrl: "/certificates/data_science_aiml_ybi.jpg",
+    downloadUrl: "/certificates/data_science_aiml_ybi.pdf"
+  },
+
+  {
+    id: "11",
     title: "Project Management Certificate – Forage",
     imageUrl: "/certificates/project_management_forage.jpg",
     downloadUrl: "/certificates/project_management_forage.pdf"
   },
-  {
-    id: "10",
-    title: "ServiceNow Micro-Certification",
-    imageUrl: "/certificates/servicenow_micro.jpeg",
-    downloadUrl: "/certificates/servicenow_micro.pdf"
-  },
-  {
-    id: "1",
-    title: "Software Development Workshop – Crio.do",
-    imageUrl: "/certificates/software_dev_workshop_crio.jpg",
-    downloadUrl: "/certificates/software_dev_workshop_crio.pdf"
-  },
-  {
+
+    {
     id: "12",
+    title: "CSS Certification – HackerRank",
+    imageUrl: "/certificates/css_hackerrank.jpg",
+    downloadUrl: "/certificates/css_hackerrank.pdf"
+  },
+
+  {
+    id: "13",
     title: "WordPress Certificate – Udemy",
     imageUrl: "/certificates/wordpress_udemy.jpg",
     downloadUrl: "/certificates/wordpress_udemy.pdf"
